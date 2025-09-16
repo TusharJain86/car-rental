8f085db63c857ba21df9c4f456720621e48fb37b
TusharJain86 <tusharjain0806@gmail.com>
2025-09-16 12:42:07 +0530

    initial setup

---
+++ Car.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Car {
+    private String carID;
+
+    private String brand;
+
+    private String model;
+
+    private double basePricePerDay;
+
+    private boolean isOnline;
+
+    //making a constructor
+}
