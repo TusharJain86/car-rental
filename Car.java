0ce18fda1ab35ffe78dc8c86424e17835d039b7d
TusharJain86 <tusharjain0806@gmail.com>
2025-09-16 13:14:52 +0530

    car class updated

--- Car.java
+++ Car.java
@@ -11,5 +11,44 @@ public class Car {
 
     private boolean isOnline;
 
-    //making a constructor
+    //<-------------making a constructor---------->
+
+    public Car(String carID, String brand, String model, double basePricePerDay){ //parameterised constructor
+        this.carID = carID;
+        this.brand = brand;
+        this.model = model;
+        this.basePricePerDay = basePricePerDay;
+        this.isOnline = true;
+        //these are the details of new car which entered our gargae
+    }
+
+    //<------------now making methods------------->
+
+    public String getCarID(){
+        return carID;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public double calculatePrice(int rentalDays){
+        return basePricePerDay * rentalDays;
+    }
+
+    public boolean isOnline(){
+        return isOnline;
+    }
+
+    public void rent(){
+        isOnline = false;
+    }
+
+    public void returncar(){
+        isOnline = true;
+    }
 }
