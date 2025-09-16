5653ece1f1d132bddd0016f43ac6cdb037712ca6
TusharJain86 <tusharjain0806@gmail.com>
2025-09-16 13:27:02 +0530

    customer class update

--- Customer.java
+++ Customer.java
@@ -1,3 +1,22 @@
+import java.util.*;
+
 public class Customer {
-    
+
+    private String customerID;
+    private String name;
+
+    //constructor
+
+    public Customer(String customerID, String name){
+        this.customerID = customerID;
+        this.name = name;
+    }
+
+    public String getCustomerID(){
+        return customerID;
+    }
+
+    public String getname(){
+        return name;
+    }
 }
