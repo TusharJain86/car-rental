536fb14e1549e6feddaf4985f40ec518c8f4fafc
TusharJain86 <tusharjain0806@gmail.com>
2025-10-23 14:50:29 +0530

    added new cars

--- Main.java
+++ Main.java
@@ -4,11 +4,15 @@ public class Main {
         CarRentalSystem rentalSystem = new CarRentalSystem();
         Car car1 = new Car("C001", "Toyota", "Camry", 60.0);
         Car car2 = new Car("C002", "Honda", "Accord", 70.0);
-        Car car3 = new Car("C003", "Mahindra", "Thar", 150.0);
+        Car car3 = new Car("C003", "Mahindra", "BE 6", 100.0);
+        Car car4 = new Car("C004", "Ford", "Mustang", 180.0);
+        Car car5 = new Car("C005", "Tata", "Safari", 150.0);
 
         rentalSystem.addCar(car1);
         rentalSystem.addCar(car2);
         rentalSystem.addCar(car3);
+        rentalSystem.addCar(car4);
+        rentalSystem.addCar(car5);
 
         javax.swing.SwingUtilities.invokeLater(() -> {
             new CarRentalGUI(rentalSystem);
